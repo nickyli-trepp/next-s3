f753022d430532872884f8d4b7c8ac987e4b5995
Nicky <nicky_li@trepp.com>
2021-06-15 15:40:42 -0400

    sync s3 on push to main

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-       <p>Test PR</p>
+       <p>Test New Changes</p>
       </main>
 
       <footer className={styles.footer}>
